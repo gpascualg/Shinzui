2d43d1fb274453769c4b5be6d4c556ccdc33ee76
gpascualg <gpascualg93@gmail.com>
2017-10-01 22:37:55 +0200

    Disable warnings for GCC

--- src/defs/common.hpp
+++ src/defs/common.hpp
@@ -16,5 +16,15 @@ using TimePoint = std::chrono::high_resolution_clock::time_point;
     #ifdef _MSC_VER
         #define INCL_NOWARN __pragma(warning(push, 0))
         #define INCL_WARN __pragma(warning(pop))
+    #else
+        #define INCL_NOWARN \
+            _Pragma(diagnostic push) \
+            _Pragma(diagnostic ignored "-Wpedantic") \
+            _Pragma(diagnostic ignored "-Wunused-variable") \
+            _Pragma(diagnostic ignored "-Wunused-result") \
+            _Pragma(diagnostic ignored "-Wstrict-aliasing") \
+            _Pragma(diagnostic ignored "-Wunreachable-code") \
+            _Pragma(diagnostic ignored "-Wuninitialized")
+        #define INCL_WARN _Pragma(diagnostic pop)
     #endif
 #endif
